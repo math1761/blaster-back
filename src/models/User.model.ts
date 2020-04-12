a3bbf4ccff75a227ba371a74213072f6b8ead951
Mathieu Levi <mathieulevi@gmail.com>
2020-04-12 21:46:36 +0200

    feat: Added user model

--- src/models/User.model.ts
+++ src/models/User.model.ts
@@ -1,9 +1,14 @@
 import mongoose from 'mongoose';
 
 const UserSchema = new mongoose.Schema({
+    pseudo: {
+        type: String,
+        required: false
+    },
     names: {
         type: String,
         required: false
     }
 });
-export const User = mongoose.model('room', UserSchema);
+
+export const User = mongoose.model('user', UserSchema);
